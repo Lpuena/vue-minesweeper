f7ee1947b810b05e8e15d73b9b4746256a03f751
rose <g2534160060@163.com>
2023-06-20 23:10:16 +0800

    :fire: add mine

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,91 @@
 <script setup lang="ts">
+interface BlockState {
+  x: number
+  y: number
+  revealed?: boolean
+  mine?: boolean
+  flagged?: boolean
+  adjacentMines: number
+}
 
+const HEIGHT = 10
+const WIDTH = 10
+const data = reactive(
+  Array.from({ length: HEIGHT }, (_, y) =>
+    Array.from({ length: WIDTH },
+      (_, x): BlockState => ({
+        x,
+        y,
+        adjacentMines: 0,
+      }),
+    ),
+  ))
+// 生成地雷
+function generateMines() {
+  for (const row of data) {
+    for (const block of row)
+      block.mine = Math.random() < 0.4
+  }
+}
+
+const directions = [
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1],
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, 1],
+]
+
+// 计算格子的数字
+function updateNumbers() {
+  data.forEach((row, y) => {
+    row.forEach((block, x) => {
+      if (block.mine)
+        return
+      directions.forEach(([dx, dy]) => {
+        const x2 = x + dx
+        const y2 = x + dy
+        if (x2 < 0 || x2 >= WIDTH || y2 < 0 || y2 >= HEIGHT)
+          return
+        if (data[y2][x2].mine)
+          block.adjacentMines++
+      })
+    })
+  })
+}
+function getBlockClass(block: BlockState) {
+  return block.mine ? 'text-red' : 'text-gray'
+}
+
+generateMines()
+updateNumbers()
+
+function onClick(x: number, y: number) {
+  // console.log(`Click at ${x},${y}`)
+}
 </script>
 
 <template>
   <div>Minesweeper</div>
-  <div>
-    <button h-5 w-5 border />
+  <div
+    v-for="(row, y) in data"
+    :key="y"
+  >
+    <button
+      v-for="(item, x) in row"
+      :key="x"
+      h-10
+      w-10
+      border
+      hover:bg-gray
+      :class="getBlockClass(item)"
+      @click="onClick(x, y)"
+    >
+      {{ item.mine ? 'x' : item.adjacentMines }}
+    </button>
   </div>
 </template>
 
